b27ccce1101dbe930981bfce6a198a20831cf8f3
San-QP <s.rebollar.g@gmail.com>
2024-03-20 10:44:59 +0100

    Calendar configurated and events added

--- frontend/src/presentation/components/ActivitiesCalendar.jsx
+++ frontend/src/presentation/components/ActivitiesCalendar.jsx
@@ -6,11 +6,20 @@ export const ActivitiesCalendar = () => {
     <FullCalendar
       plugins={[timeGridPlugin]}
       initialView='timeGridWeek'
+      locale= "esLocale"
+      firstDay= {1}
+      themeSystem='Minty'
       headerToolbar={{
         left: 'prev,next',
         center: 'title',
-        right: 'timeGridWeek,timeGridDay'
+        right: 'timeGridWeek,timeGridDay',
       }}
+      timeZone= 'GMT+1'
+      events= {[
+        {title: 'The Title', // a property!
+          start: '2024-03-20T10:00:00.000Z', // a property!
+          end: '2024-03-20T11:00:00.000Z' // a property! ** see important note below about 'end' **
+        }]}
     />
   );
 };
