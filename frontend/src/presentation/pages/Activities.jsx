28727ef5ff34e1902eb4d274da33287dea8a8c5b
herminiocastro <hxssport@gmail.com>
2024-03-20 15:00:19 +0100

    header finish

--- frontend/src/presentation/pages/Activities.jsx
+++ frontend/src/presentation/pages/Activities.jsx
@@ -1,8 +1,10 @@
 import { ActivitiesCalendar } from "../components/ActivitiesCalendar"
+import { Header } from "./Header.jsx"
 
 export const Activities = () => {
   return (
     <>
+    <Header />
     <ActivitiesCalendar />
     </>
   )
